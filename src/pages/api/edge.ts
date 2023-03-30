985ca0c1665dcbcbe2e4a0aad4b1d51f351745b6
Mukitul Islam Mukit <hellomukit@gmail.com>
2023-03-30 07:19:08 +0600

    feat: fetch data using planetscale database driver

--- src/pages/api/edge.ts
+++ src/pages/api/edge.ts
@@ -1,13 +1,27 @@
-import type { NextRequest } from "next/server";
+import { connect } from "@planetscale/database";
 
 export const config = {
   runtime: "edge",
 };
 
-export default function handler() {
+const dbConfig = {
+  host: "aws.connect.psdb.cloud",
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+};
+
+export default async function handler() {
+  const conn = connect(dbConfig);
+
+  const results = await conn.execute(
+    "SELECT `mitest`.`example`.`id`, `mitest`.`example`.`name`, `mitest`.`example`.`createdAt`, `mitest`.`example`.`updatedAt` FROM `mitest`.`example` WHERE 1=1 ORDER BY `mitest`.`example`.`id` ASC LIMIT ? OFFSET ?",
+    [100, 0]
+  );
+
   return new Response(
     JSON.stringify({
       message: "Hello from Edge runtime!",
+      data: results?.rows ? results.rows : [],
     }),
     {
       status: 200,
--- src/pages/api/node.ts
+++ src/pages/api/node.ts
@@ -1,5 +1,25 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import { connect } from "@planetscale/database";
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json({ message: "Hello from NodeJS runtime!" });
+const dbConfig = {
+  host: "aws.connect.psdb.cloud",
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const conn = connect(dbConfig);
+
+  const results = await conn.execute(
+    "SELECT `mitest`.`example`.`id`, `mitest`.`example`.`name`, `mitest`.`example`.`createdAt`, `mitest`.`example`.`updatedAt` FROM `mitest`.`example` WHERE 1=1 ORDER BY `mitest`.`example`.`id` ASC LIMIT ? OFFSET ?",
+    [100, 0]
+  );
+
+  res.status(200).json({
+    message: "Hello from NodeJS runtime!",
+    data: results?.rows ? results.rows : [],
+  });
 }
